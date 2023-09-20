fb5608d222084860aca6819a7f79d57d88dc6bbe
Nora Sadder <norasadder12@gmail.com>
2023-09-20 11:59:18 +0300

    full task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function FilterableMemberTable() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="filterableMemberTable">
+      <SearchBar />
+      <MembersTable names={NAMES} />
+      <ActionButtons />
     </div>
   );
 }
 
-export default App;
+function SearchBar() {
+  return (
+    <div className="searchBar">
+      <img src={require("./search.png")}></img>
+      <input type="text" placeholder="Find members"></input>
+    </div>
+  );
+}
+
+function MembersTable({ names }) {
+  const rows = [];
+  names.forEach((name) => {
+    rows.push(<MemberRow memberName={name} key={name} />);
+  });
+  return <div>{rows}</div>;
+}
+
+function MemberRow({ memberName }) {
+  return (
+    <div className="memberRow">
+      <MemberData memberName={memberName} />
+      <RoundedCheckbox id={memberName} />
+    </div>
+  );
+}
+
+function MemberData({ memberName }) {
+  return (
+    <div className="memberData">
+      <img src={require("./user-gray.png")} className="memberIcon"></img>
+      <p>{memberName}</p>
+    </div>
+  );
+}
+
+function RoundedCheckbox({ id }) {
+  return (
+    <div className="checkbox-container">
+      <input type="checkbox" id={id}></input>
+      <label htmlFor={id}></label>
+    </div>
+  );
+}
+
+function ActionButtons() {
+  return (
+    <div className="actionButtons">
+      <button className="cancelButton">Cancel</button>
+      <button className="saveButton">Save</button>
+    </div>
+  );
+}
+
+const NAMES = [
+  "Carolien Bloeme",
+  "Sun Jun",
+  "Song Bad",
+  "Olivia Arribas",
+  "Bonginkosi Mdiodiana",
+  "Arina Belomestnykh",
+  "Jacqueline Likoki",
+];
+
+export default function App() {
+  return (
+    <div>
+      <p className="header">Add members to Front-end development team</p>
+      <FilterableMemberTable />
+    </div>
+  );
+}
